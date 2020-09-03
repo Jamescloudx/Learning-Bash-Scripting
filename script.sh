ab0d492bb02966869241d95228765428f4fea658
Jamescloudx <udembajames@gmail.com>
2020-09-03 12:21:58 +0100

    Module-3

--- script.sh
+++ script.sh
@@ -15,8 +15,28 @@ school='Yaba College of Technology Lagos'
 echo school i attended was $school
 
 Course='Public Administration'
-echo course i studied was $course 
+# echo course i studied was $course 
 
-Founded=1014
-echo it was founded in $Founded
+# Founded=1014
+# echo it was founded in $Founded
 
+
+# read -p "Enter name: " $name
+
+# read -p "Enter School: " $school
+
+# read -p "bae: " $bae
+
+# read -p "enter club: " $club
+
+# read -p  "Best Artist: " $Best_Artist
+
+# read -p "My country: " $country
+
+# echo my name is $name my school is $school bae is cruise $bae my club is $club best artist is $artist my country is $country
+
+read -p "username for https://github.com: " username
+
+read -sp "password for https://"$username"@github.com: " password
+
+echo "this is the" $username and $password
